22f44ef9d601f4f2c719cf611c599821209c7607
CristianLasso <47828389+CristianLasso@users.noreply.github.com>
2021-10-30 10:51:04 -0500

    SearchBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import ForumPage from "./pages/ForumPage/ForumPage"
+import PrimarySearchAppBar from './components/PrimarySearchAppBar';
 import React from 'react';
 
 
 function App() {
   return (
     <div className="App">
+      <PrimarySearchAppBar />
       <ForumPage />
     </div>
   );
